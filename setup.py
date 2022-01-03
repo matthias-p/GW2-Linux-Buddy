3103bd2ec5c91b24dd2addbed7b773c5166de895
Matthias Pijarowski <pijarowski.matthias@gmail.com>
2022-01-03 13:57:54 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gw2_linux_buddy',
+    version='0.1',
+    packages=['game_utils', 'game_utils.exceptions', 'lb_settings', 'user_interface', 'user_interface.widgets'],
+    url='https://github.com/matthias-p/GW2-Linux-Buddy',
+    license='MIT',
+    author='matthias',
+    author_email='pijarowski.matthias@gmail.com',
+    description='GW2-Linux_buddy'
+)
